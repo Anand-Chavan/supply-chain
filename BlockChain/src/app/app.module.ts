65ba2d0590788e9d1d4752188d8c713627d6dd80
Anand Chavan <anand2431997@gmail.com>
2020-06-26 19:43:54 +0530

    Added front end

---
+++ BlockChain/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { RetailerComponent } from './retailer/retailer.component';
+import { MiddlewareComponent } from './middleware/middleware.component';
+import { ShopkeererComponent } from './shopkeerer/shopkeerer.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule }   from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import {MatTabsModule} from '@angular/material/tabs'; 
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RetailerComponent,
+    MiddlewareComponent,
+    ShopkeererComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatCardModule,
+    MatInputModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatTabsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
